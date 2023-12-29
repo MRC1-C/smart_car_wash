11b9142bd8938aa0c77cf38a870b0eb9bb19faf1
quando <quanchopper1234@gmail.com>
2023-12-29 23:10:41 +0700

    push

--- next.config.js
+++ next.config.js
@@ -9,8 +9,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
-                source: "/api/:path*",
+                source: "/api/*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
